142743fd4552fc74502b43eb17060799c8aad287
David Borge <david.borge.olmedo@gmail.com>
2023-04-11 18:20:22 +0200

    Corrección menor de CORS

--- getAllProducts.php
+++ getAllProducts.php
@@ -1,7 +1,7 @@
 <?php
 // Prevenir que falle la llamada a la API por la CORS Policy porque la web está en un dominio (https://online-store.davidborge.com o http://localhost:4200) y la API en otro (davidborge.com)
 $http_origin = $_SERVER['HTTP_ORIGIN'];
-if ($http_origin == "http://localhost:4200" || $http_origin == "http://localhost:4000" || $http_origin == "https://online-store.davidborg0" || $http_origin == "https://online-store.davidborge.com" || $http_origin == "https://online-store-web.herokuapp.com" || $http_origin == "https://online-store-ssrr.davidborge.com" )
+if ($http_origin == "http://localhost:4200" || $http_origin == "http://localhost:4000" || $http_origin == "https://online-store.davidborg0" || $http_origin == "https://online-store.davidborge.com" || $http_origin == "https://online-store-web.herokuapp.com" || $http_origin == "https://online-store-ssr.davidborge.com" )
 {  
     header("Access-Control-Allow-Origin: $http_origin");
 }
