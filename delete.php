d0f47ad7240d1e5c30417bc25eefd6b1b677aaf6
David Borge <david.borge.olmedo@gmail.com>
2023-04-18 13:44:48 +0200

    Corregir error 500

--- delete.php
+++ delete.php
@@ -1,7 +1,8 @@
 <?php
 // Prevenir que falle la llamada a la API por la CORS Policy porque la web está en un dominio (https://online-store.davidborge.com o http://localhost:4200) y la API en otro (davidborge.com)
+// MUCHO CUIDADO: si se me olvida poner la extensión del dominio en una de las alternativas, provoca un Error 500.
 $http_origin = $_SERVER['HTTP_ORIGIN'];
-if ($http_origin == "http://localhost:4200" || $http_origin == "http://localhost:4000" || $http_origin == "https://online-store.davidborg0" || $http_origin == "https://online-store.davidborge.com" || $http_origin == "https://online-store-web.herokuapp.com" || $http_origin == "https://online-store-ssr.davidborge.com" )
+if ($http_origin == "http://localhost:4200" || $http_origin == "http://localhost:4000" || $http_origin == "https://online-store.davidborge.com" || $http_origin == "https://online-store-web.herokuapp.com" || $http_origin == "https://online-store-ssr.davidborge.com" )
 {  
     header("Access-Control-Allow-Origin: $http_origin");
 }
