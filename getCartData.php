5cbec1330a9308bee82ddd784f500471f7d25ec9
David Borge <david.borge.olmedo@gmail.com>
2023-05-12 20:53:54 +0200

    Cart: load data from Data Base

---
+++ getCartData.php
@@ -0,0 +1,39 @@
+<?php
+// Prevenir que falle la llamada a la API por la CORS Policy porque la web está en un dominio (https://online-store.davidborge.com o http://localhost:4200) y la API en otro (davidborge.com)
+// MUCHO CUIDADO: si se me olvida poner la extensión del dominio en una de las alternativas, provoca un Error 500.
+$http_origin = $_SERVER['HTTP_ORIGIN'];
+if ($http_origin == "http://localhost:4200" || $http_origin == "http://localhost:4000" || $http_origin == "https://online-store.davidborge.com" || $http_origin == "https://online-store-web.herokuapp.com" || $http_origin == "https://online-store-ssr.davidborge.com" )
+{  
+    header("Access-Control-Allow-Origin: $http_origin");
+}
+
+header("Access-Control-Allow-Headers: *");
+
+$jsonAPIPayload = json_decode(file_get_contents("php://input"));
+if (!$jsonAPIPayload) {
+    exit("GET_CART_DATA_ERROR_API_DID_NOT_RECIEVE_ITS_PAYLOAD");
+}
+$bd = include_once "bd.php";
+
+try {
+
+    // - API Payload (email y contraseña introducidos en el formulario de Log In, lastLoginFullDate y el token (que cambia cada vez que se inicia sesión), que es la fecha actual)
+    $authTokenAPIPayload = $jsonAPIPayload->authToken;
+
+    // Leer desde la Base de Datos el Cart Data del usuario con el token $authTokenAPIPayload
+    $sentencia = $bd->prepare("SELECT cart.productId, cart.productQuantity, products.name, products.price, products.imageThumbnail, products.imageWidth, products.imageHeight FROM cart, products, users WHERE cart.productId = products.id AND cart.userId = users.id AND users.token = ?");
+    $sentencia->execute([$authTokenAPIPayload]);
+    $resultado = $sentencia->fetchAll(PDO::FETCH_OBJ);
+    
+    echo json_encode($resultado);
+
+} catch (Exception $e) {
+
+    // exception is raised and it'll be handled here 
+    // $e->getMessage() contains the error message
+
+    echo json_encode([
+        "resultado" => $e->getMessage(), // Ejemplo: "SQLSTATE[23000]: Integrity constraint violation: 1062 Duplicate entry 'hewemim@mailinator.com' for key 'users.email'"
+    ]);
+
+}
