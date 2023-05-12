daa76d80799730c6b917f706d0a82d9d577d10c7
David Borge <david.borge.olmedo@gmail.com>
2023-05-12 15:16:19 +0200

    Get All Countries

---
+++ getAllCountries.php
@@ -0,0 +1,13 @@
+<?php
+// Prevenir que falle la llamada a la API por la CORS Policy porque la web está en un dominio (https://online-store.davidborge.com o http://localhost:4200) y la API en otro (davidborge.com)
+// MUCHO CUIDADO: si se me olvida poner la extensión del dominio en una de las alternativas, provoca un Error 500.
+$http_origin = $_SERVER['HTTP_ORIGIN'];
+if ($http_origin == "http://localhost:4200" || $http_origin == "http://localhost:4000" || $http_origin == "https://online-store.davidborge.com" || $http_origin == "https://online-store-web.herokuapp.com" || $http_origin == "https://online-store-ssr.davidborge.com" )
+{  
+    header("Access-Control-Allow-Origin: $http_origin");
+}
+
+$bd = include_once "bd.php";
+$sentencia = $bd->query("SELECT * FROM countries");
+$countries = $sentencia->fetchAll(PDO::FETCH_OBJ);
+echo json_encode($countries);
