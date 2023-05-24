ec1e0cea7e07c07a88691371e442778403bc8787
David Borge <david.borge.olmedo@gmail.com>
2023-05-24 18:20:59 +0200

    CORS: cualquier dirección

--- _delete.php
+++ _delete.php
@@ -2,7 +2,8 @@
 // Prevenir que falle la llamada a la API por la CORS Policy porque la web está en un dominio (https://online-store.davidborge.com o http://localhost:4200) y la API en otro (davidborge.com)
 // MUCHO CUIDADO: si se me olvida poner la extensión del dominio en una de las alternativas, provoca un Error 500.
 $http_origin = $_SERVER['HTTP_ORIGIN'];
-if ($http_origin == "http://localhost:4200" || $http_origin == "http://localhost:4000" || $http_origin == "https://online-store.davidborge.com" || $http_origin == "https://online-store-web.herokuapp.com" || $http_origin == "https://online-store-ssr.davidborge.com" || $http_origin == "http://192.168.1.43:4200"  )
+if ( true )
+// if ($http_origin == "http://localhost:4200" || $http_origin == "http://localhost:4000" || $http_origin == "https://online-store.davidborge.com" || $http_origin == "https://online-store-web.herokuapp.com" || $http_origin == "https://online-store-ssr.davidborge.com" || $http_origin == "http://192.168.1.43:4200"  )
 {  
     header("Access-Control-Allow-Origin: $http_origin");
 }
