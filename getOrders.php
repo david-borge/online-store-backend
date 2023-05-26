ae5b91a2ca8c77047d8deab3f7872dc2a9ba53b6
David Borge <david.borge.olmedo@gmail.com>
2023-05-26 15:58:51 +0200

    Fix: get only the orders of the loggin user

--- getOrders.php
+++ getOrders.php
@@ -3,7 +3,6 @@
 // MUCHO CUIDADO: si se me olvida poner la extensión del dominio en una de las alternativas, provoca un Error 500.
 $http_origin = $_SERVER['HTTP_ORIGIN'];
 if ( true )
-// if ( true )
 // if ($http_origin == "http://localhost:4200" || $http_origin == "http://localhost:4000" || $http_origin == "https://online-store.davidborge.com" || $http_origin == "https://online-store-web.herokuapp.com" || $http_origin == "https://online-store-ssr.davidborge.com" || $http_origin == "http://192.168.1.43:4200"  )
 {  
     header("Access-Control-Allow-Origin: $http_origin");
@@ -42,7 +41,7 @@ try {
 
             echo json_encode([
                 "resultado" => true,
-                "orders"    => $resultado,
+                "orders"    => $resultado2,
             ]);
 
         }
